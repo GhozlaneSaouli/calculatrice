6888d0b4c638318edf3a58a281c94560a4ad591b
GhozlaneSaouli <saouli.ghozlane@gmail.com>
2022-11-20 21:54:10 +0100

    add handleClick

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-const ddd = document.getElementsByClassName("ce");
-console.log({ dd });
+function handleClick(value) {
+  switch (value) {
+    case "ce":
+      document.getElementById("display").innerText = "";
+      break;
+    case "←":
+      document.getElementById("display").innerText = document
+        .getElementById("display")
+        .innerText.slice(
+          0,
+          document.getElementById("display").innerText.length - 1
+        );
+      break;
+    case "=":
+      console.log(document.getElementById("display").innerText);
+
+      if (document.getElementById("display").innerText.length === 0) {
+        return;
+      }
+
+      document.getElementById("display").innerText = eval(
+        document.getElementById("display").innerText
+      );
+      break;
+
+    default:
+      document.getElementById("display").innerText =
+        document.getElementById("display").innerText + value;
+      break;
+  }
+}
